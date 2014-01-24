d5701e6508bc82af823fa74bb3579997a05e68c6
edward <edward@localhost>
2014-01-24 01:52:26 -0500

    Added the TextAreaPrintStream class which inherits from the PrintStream class to print to the JTextArea on the applet. Cleaned up code for the applet as well

---
+++ ImageScrapper/src/Interface/TextAreaPrintStream.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+import java.io.PrintStream;
+import java.io.OutputStream;
+import javax.swing.JTextArea;
+/**
+ *
+ * @author edward
+ */
+public class TextAreaPrintStream extends PrintStream {
+
+    private JTextArea textarea;
+    
+    public TextAreaPrintStream(JTextArea area, OutputStream output) {
+        super(output);
+        textarea = area;
+    }
+    
+    @Override
+    public void println(String str) {
+       textarea.append((str + '\n'));
+    }
+    
+    @Override
+    public void print(String str) {
+       textarea.append(str);
+    }
+}
