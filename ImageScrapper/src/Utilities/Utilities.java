60e452bdc4419b5ae22e981d0746b73c7d4ba01f
Edward Kim <edbeta12@gmail.com>
2014-06-27 15:02:59 -0400

    Removed Unessescary static string variables for directory in question and link,img rule expressions. Implemented new command passing system

--- ImageScrapper/src/Utilities/Utilities.java
+++ ImageScrapper/src/Utilities/Utilities.java
@@ -1,9 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package imagescrapper;
+package Utilities;
 
 /**
  *
@@ -12,30 +7,9 @@ package imagescrapper;
 
 import java.io.File;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
-import javax.swing.JTextField;
-import java.io.IOException; 
-
-public class Utility {
+public class Utilities {
     
-   //Utility class: contains methods needed universally in the program
-    public static String url;
-    public static JTextField imgexp;
-    public static JTextField linkexp;
-    public static String directory;
-    
-    //needed for linux and windows file system differentiation.
-    public static final String OS = System.getProperty("os.name").toLowerCase();
-    public static String delimiter = (OS.indexOf("win") >= 0) ? "\\" : "/";
-    
-    public static Elements getElements(String tags, String url) throws IOException {
-        
-         Document doc = Jsoup.connect(url).userAgent("Mozilla").get();
-         return doc.select(tags);
-    }
+    //Utility class: contains methods needed universally in the program
     
      public static boolean CheckifDirExists(String path) {
         return (new File(path)).exists();
@@ -44,6 +18,10 @@ public class Utility {
      public static String escapeBackSlash(String path) {
          //escapes backslashes in directory pathnames 
          return path.replaceFirst("\\\\","\\\\\\\\");
+     }
+     
+     public static String getLastStringFromUrl(String url) {
+        return url.substring(url.lastIndexOf("/") + 1);
      }
 
 }
